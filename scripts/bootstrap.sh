952d9f43f9056dbc44c7b97cda0f6246e7cb94f6
ehc-io <eduardo@ehc.io>
2019-04-20 10:36:46 -0300

    reset database for every run

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Bootstraping 
 tar -zxf /loopback-folder/scripts/loopback-dep.tar.gz -C /loopback-folder
-mongoimport --host mongodb.local --port 27017 --username admin --password admin --authenticationDatabase admin -d bookstoreappdb -c full-book-catalog-model /loopback-folder/dataset/catalog.books.json
+mongo admin --host mongodb.local:27017 -u admin -p admin < /loopback-folder/scripts/dropdatabase.js
+mongoimport --host mongodb.local --port 27017 --username admin --password admin --authenticationDatabase admin -d bookstoreappdb -c full_book_catalog /loopback-folder/dataset/catalog.books.json
 mongo admin --host mongodb.local:27017 -u admin -p admin < /loopback-folder/scripts/mongosetup.js
 
