3a244ec256dd165947e80f2f7a12ab8597d10ee5
ehc-io <eduardo@ehc.io>
2019-07-07 15:50:54 -0300

    vm startup script

--- scripts/ubuntu_install.sh
+++ scripts/ubuntu_install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+screen -q
 sudo apt-get update
 sudo apt-get install -y docker docker-compose
+cd ~
 git clone https://github.com/ehc-io/bookscatalog.git
+cd bookscatalog/
+docker-compose up
