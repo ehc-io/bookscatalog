f838cdc78ef165588d66f50e742ac16a5295f178
ehc-io <eduardo@ehc.io>
2019-07-07 16:24:12 -0300

    multiple cmd line

--- scripts/ubuntu_install.sh
+++ scripts/ubuntu_install.sh
@@ -3,6 +3,6 @@ screen -q
 cd /home/f5student
 sudo apt-get update
 sudo apt-get install -y docker docker-compose
-git clone https://github.com/ehc-io/bookscatalog.git
-cd bookscatalog/
-sudo docker-compose up
+git clone https://github.com/ehc-io/bookscatalog.git ;
+cd bookscatalog/ ;
+sudo docker-compose up 
