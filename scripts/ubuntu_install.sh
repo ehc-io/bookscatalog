ab167e852be62c3558da259270109d44471a48f2
ehc-io <eduardo@ehc.io>
2019-07-07 16:17:57 -0300

    clean up

--- scripts/ubuntu_install.sh
+++ scripts/ubuntu_install.sh
@@ -3,7 +3,6 @@ screen -q
 cd /home/f5student
 sudo apt-get update
 sudo apt-get install -y docker docker-compose
-cd ~
 git clone https://github.com/ehc-io/bookscatalog.git
 cd bookscatalog/
 docker-compose up
